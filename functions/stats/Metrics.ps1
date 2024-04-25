60da855d77a309188dd687d628561a22bbd09c8c
Mike Madeja <mike@madeja.us>
2024-04-25 00:14:33 -0500

    updated metrics

--- functions/stats/Metrics.ps1
+++ functions/stats/Metrics.ps1
@@ -1,4 +1,23 @@
 function Get-PiHoleStatsRecentBlocked {
+    <#
+.SYNOPSIS
+https://ftl.pi-hole.net/development-v6/docs/#get-/stats/recent_blocked
+
+.PARAMETER PiHoleServer
+The URL to the PiHole Server, for example "http://pihole.domain.com:8080", or "http://192.168.1.100"
+
+.PARAMETER Password
+The API Password you generated from your PiHole server
+
+.PARAMETER MaxResult
+How many results should be returned
+
+.PARAMETER RawOutput
+This will dump the response instead of the formatted object
+
+.EXAMPLE
+Get-PiHoleStatsRecentBlocked -PiHoleServer "http://pihole.domain.com:8080" -Password "fjdsjfldsjfkldjslafjskdl" -MaxResult 20
+    #>
     [CmdletBinding()]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "Password")]
     param (
@@ -10,16 +29,15 @@ function Get-PiHoleStatsRecentBlocked {
     try {
         $Sid = Request-PiHoleAuth -PiHoleServer $PiHoleServer -Password $Password
         Write-Verbose -Message "MaxResults - $MaxResult"
-        
         $Params = @{
             Headers     = @{sid = $($Sid) }
             Uri         = "$PiHoleServer/api/stats/recent_blocked?count=$MaxResult"
             Method      = "Get"
             ContentType = "application/json"
-        } 
-        
+        }
+
         $Response = Invoke-RestMethod @Params
-    
+
         if ($RawOutput) {
             Write-Output $Response
         }
@@ -27,7 +45,7 @@ function Get-PiHoleStatsRecentBlocked {
             $ObjectFinal = @()
             foreach ($Item in $Response.blocked) {
                 $Object = $null
-                $Object = [PSCustomObject]@{ 
+                $Object = [PSCustomObject]@{
                     Blocked = $Item
                 }
                 Write-Verbose -Message "Blocked - $Item"
