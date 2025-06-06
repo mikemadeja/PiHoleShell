dab2ff0c30f7d3e51e25ad29ca7384f8e9c78823
Mike Madeja <mike@madeja.us>
2025-06-06 00:14:09 -0500

    require powershell 7

--- PiHoleShell.psm1
+++ PiHoleShell.psm1
@@ -1,4 +1,7 @@
-#Requires -Version 5.0
+if (-not $PSVersionTable -or $PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Error "This script requires PowerShell 7 or higher. Current version: $($PSVersionTable.PSVersion)"
+    exit 1
+}
 
 # Import all the commands
 Get-ChildItem $PSScriptRoot\functions\*.ps1 -Recurse | ForEach-Object { . $_.FullName }
