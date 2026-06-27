1146f71e6b4aa33b19b378883a2fabbf72dcf8cc
Mike Madeja <mike@madeja.us>
2026-06-27 07:57:08 -0500

    debugging

--- PiHoleShell/Public/ListManagement/Search-PiHoleListDomain.ps1
+++ PiHoleShell/Public/ListManagement/Search-PiHoleListDomain.ps1
@@ -1,7 +1,7 @@
 function Search-PiHoleListDomain {
     <#
 .SYNOPSIS
-Search domains in Pi-hole's lists
+https://ftl.pi-hole.net/master/docs/#get-/search/-domain-
 
 .PARAMETER PiHoleServer
 The URL to the PiHole Server, for example "http://pihole.domain.com:8080", or "http://192.168.1.100"
@@ -9,14 +9,24 @@ The URL to the PiHole Server, for example "http://pihole.domain.com:8080", or "h
 .PARAMETER Password
 The API Password you generated from your PiHole server
 
+.PARAMETER Domain
+The domain to search for in Pi-hole's lists
+
+.PARAMETER PartialMatch
+Set to $true to include partial matches, defaults to $false (exact matches only)
+
+.PARAMETER MaxResults
+Maximum number of results to return, defaults to 20
+
 .PARAMETER IgnoreSsl
 Set to $true to skip SSL certificate validation
 
 .PARAMETER RawOutput
 This will dump the response instead of the formatted object
 
+.EXAMPLE
+Search-PiHoleListDomain -PiHoleServer "http://pihole.domain.com:8080" -Password "fjdsjfldsjfkldjslafjskdl" -Domain "doubleclick.net"
     #>
-    #Work In Progress
     [CmdletBinding(HelpUri = 'https://ftl.pi-hole.net/master/docs/#get-/search/-domain-')]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "Password")]
     param (
@@ -37,7 +47,6 @@ This will dump the response instead of the formatted object
         $Body = @{
             n       = $MaxResults
             partial = $PartialMatch
-            name    = $GroupName
         }
 
         $Params = @{
@@ -57,7 +66,43 @@ This will dump the response instead of the formatted object
 
         else {
             $ObjectFinal = @()
-            foreach ($Item in $Response.lists) {
+            foreach ($Item in $Response.search.gravity) {
+                $Object = [PSCustomObject]@{
+                    Domain         = $Item.domain
+                    Address        = $Item.address
+                    Comment        = $Item.comment
+                    Enabled        = $Item.enabled
+                    Id             = $Item.id
+                    Type           = $Item.type
+                    Groups         = $Item.groups
+                    DateAdded      = (Convert-PiHoleUnixTimeToLocalTime -UnixTime $Item.date_added).LocalTime
+                    DateModified   = (Convert-PiHoleUnixTimeToLocalTime -UnixTime $Item.date_modified).LocalTime
+                    DateUpdated    = (Convert-PiHoleUnixTimeToLocalTime -UnixTime $Item.date_updated).LocalTime
+                    Number         = $Item.number
+                    InvalidDomains = $Item.invalid_domains
+                    AbpEntries     = $Item.abp_entries
+                    Status         = $Item.status
+                }
+                $ObjectFinal += $Object
+            }
+            foreach ($Item in $Response.search.domains) {
+                $Object = [PSCustomObject]@{
+                    Domain         = $Item.domain
+                    Address        = $null
+                    Comment        = $Item.comment
+                    Enabled        = $Item.enabled
+                    Id             = $Item.id
+                    Type           = $Item.type
+                    Groups         = $Item.groups
+                    DateAdded      = (Convert-PiHoleUnixTimeToLocalTime -UnixTime $Item.date_added).LocalTime
+                    DateModified   = (Convert-PiHoleUnixTimeToLocalTime -UnixTime $Item.date_modified).LocalTime
+                    DateUpdated    = $null
+                    Number         = $null
+                    InvalidDomains = $null
+                    AbpEntries     = $null
+                    Status         = $Item.status
+                }
+                $ObjectFinal += $Object
             }
             Write-Output $ObjectFinal
         }
